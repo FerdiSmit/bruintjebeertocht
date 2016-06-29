cf984eb3f5e4fcb8eaacf74d7c10cd7bb32e2065
FerdiSmit <ferdismit89@live.nl>
2016-06-29 13:20:36 +0200

    Project changes

--- layout/header.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <title><?php if (isset($title)) {echo $title; } ?></title>
-        <link rel="stylesheet" href="../css/bootstrap.css">
-        <link rel="stylesheet" href="../css/main.css">
-    </head>
-    <body>
