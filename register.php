c8eb16d83862b3879ebed8c939d96f7891025d96
FerdiSmit <ferdismit89@live.nl>
2016-07-05 10:21:02 +0200

    Info blijft staan in de ingevulde velden als er een foutmelding is

--- register.php
+++ register.php
@@ -11,27 +11,33 @@ if (isset($_POST['submit']))
 <div class="container">
     <div class="row">
         <div class="col-xs-12">
-            <h2>Registreren</h2>
+            <h2>Registreer Gebruiker</h2>
             <hr/>
         </div>
     </div>
-    <form role="form" method="post" action="" class="form-horizontal">
+    <form role="form" method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" class="form-horizontal">
         <div class="row">
             <div class="form-group">
                 <label for="username" class="col-xs-12 col-sm-2 control-label">Gebruikersnaam</label>
                 <div class="col-xs-12 col-sm-6">
-                    <input type="text" class="form-control" name="username" id="username" placeholder="Gebruikersnaam...">
+                    <input type="text" class="form-control" name="username" id="username" placeholder="Gebruikersnaam..." value="<?php if (isset($_POST['username'])) { echo htmlspecialchars($_POST['username']); } ?>">
                     <?php
-                    echo '<span class="error">' . $usernameErr . '</span>';
+                    if (isset($usernameErr))
+                    {
+                        echo '<span class="error">' . $usernameErr . '</span>';
+                    }
                     ?>
                 </div>
             </div>
             <div class="form-group">
                 <label for="email" class="col-sm-2 control-label">Email</label>
                 <div class="col-sm-6">
-                    <input type="text" name="email" class="form-control" id="email" placeholder="Email...">
+                    <input type="text" name="email" class="form-control" id="email" placeholder="Email..." value="<?php if (isset($_POST['email'])) { echo htmlspecialchars($_POST['email']); } ?>">
                     <?php
-                    echo '<span class="error">' . $emailErr . '</span>';
+                    if (isset($emailErr))
+                    {
+                        echo '<span class="error">' . $emailErr . '</span>';
+                    }
                     ?>
                 </div>
             </div>
@@ -40,7 +46,10 @@ if (isset($_POST['submit']))
                 <div class="col-sm-6">
                     <input type="password" name="password" class="form-control" id="password" placeholder="Wachtwoord...">
                     <?php
-                    echo '<span class="error">' . $passErr . '</span>';
+                    if (isset($passErr))
+                    {
+                        echo '<span class="error">' . $passErr . '</span>';
+                    }
                     ?>
                 </div>
             </div>
@@ -49,7 +58,10 @@ if (isset($_POST['submit']))
                 <div class="col-sm-6">
                     <input type="password" name="confirmPassword" class="form-control" id="confirmPassword" placeholder="Bevestig wachtwoord...">
                     <?php
-                    echo '<span class="error">' . $confPassErr . '</span>';
+                    if (isset($confPassErr))
+                    {
+                        echo '<span class="error">' . $confPassErr . '</span>';
+                    }
                     ?>
                 </div>
             </div>
